d09fb3f90c363993289cba8f422dcd548b443c77
bmoxey <bmoxey@gmail.com>
2023-08-05 10:01:27 +1000

    Initial Commit

---
+++ SwiftUI Slots/SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SlotsApp.swift
+//  SwiftUI Slots
+//
+//  Created by Brett Moxey on 5/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
